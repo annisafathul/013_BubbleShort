7824d81d9b075e8102ff9420168b8d40c64653ba
annisafathul <annisafathul18@gmail.com>
2026-03-05 15:48:23 +0700

    menampilkan data pada number

--- bubble_shortA.cpp
+++ bubble_shortA.cpp
@@ -33,3 +33,29 @@ void input()
         cin >> arr [i];
     }
 }
+
+void bubbleSortArray()
+{
+    int pass = 1; // step 1
+
+    do
+    {
+        for (int j = 0; j <= n - 1 - pass; j++)
+        { // step 2
+            if (arr[j] > arr[j + 1])
+            {
+                int temp;
+                temp = arr[j];
+                arr[j + 1] = temp;
+            }
+        
+    
+        }
+        pass += 1;
+
+        cout << "\nPass " << pass - 1 << ": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " "; // Menampilkan data pada number
+        }
+        cout << endl;
