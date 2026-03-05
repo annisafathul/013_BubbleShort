25ad31ed0ea0b4be804c5d98977a85bbef0c95af
annisafathul <annisafathul18@gmail.com>
2026-03-05 15:31:52 +0700

    Memasukkan elemen array

--- bubble_shortA.cpp
+++ bubble_shortA.cpp
@@ -22,4 +22,14 @@ void input()
         }
 
     }
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "======================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr [i];
+    }
 }
