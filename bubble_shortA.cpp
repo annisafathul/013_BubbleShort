f2e67980646393ad571e7b152ac93a39ac309a06
annisafathul <annisafathul18@gmail.com>
2026-03-05 15:57:18 +0700

    menampilkan array

--- bubble_shortA.cpp
+++ bubble_shortA.cpp
@@ -59,3 +59,22 @@ void bubbleSortArray()
             cout << arr[k] << " "; // Menampilkan data pada number
         }
         cout << endl;
+    } while (pass <= n - 1); 
+}
+
+void display()
+{
+    cout << endl;
+    cout << "===================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "===================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j]; 
+        if (j < n -1)
+        {
+            cout << "--> ";
+        }
+    }
+}
